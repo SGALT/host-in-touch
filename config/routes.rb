c32c6354aa93d1f08259b61f7047c134d58f8b46
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2021-12-22 22:19:12 +0100

    routes to manage sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:create]
+  get :login, to: "sessions#new", as: "new_hotel_session"
+  delete :logout, to: "sessions#destroy", as: "destroy_session"
 end
