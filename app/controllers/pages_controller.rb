488275d88cb71e617a2fa201e5d18777b42b0bcd
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2021-12-22 22:22:24 +0100

    test auth and fetch data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
+  require 'open-uri'
+  require 'nokogiri'
+  require 'rest-client'
+
   def home
+  end
+  
+  def rooms
+    url = "http://localhost:3000/api/v1/rooms"
+    puts cookies[:jwt_roomintouch]
+    response = RestClient.get(url, headers={Authorization: cookies[:jwt_roomintouch]})
+    @rooms = JSON.parse(response)
   end
 end
